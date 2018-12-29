e3f61bcd75d20c1ddb29c7aaf10565003d90b026
katerina <sk.katherine@gmail.com>
2018-12-29 17:53:46 +0200

    minor changes

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-controls',
@@ -6,6 +7,17 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./controls.component.scss']
 })
 export class ControlsComponent implements AfterViewInit {
+  
+  CONTROLS: Array<string> = ['up', 'down', 'left', 'right'];
+  CONTROL_CODES: Array<number> = [38, 40, 37, 39];
+
+  @ViewChild('controller') controller: ElementRef;
+  @ViewChild('trainStatus') trainStatus: ElementRef;
+  @ViewChild('learningRate') learningRate: ElementRef;
+  @ViewChild('batchSizeFraction') batchSizeFraction: ElementRef;
+  @ViewChild('epochs') epochs: ElementRef;
+  @ViewChild('denseUnits') denseUnits: ElementRef;
+  @ViewChild('status') status: ElementRef;
 
   constructor() { }
 
