1ed44c6977379f846d1b54f468522a8193532474
katerina <sk.katherine@gmail.com>
2018-12-29 20:06:55 +0200

    added home screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: "**", redirectTo: "home" },
+  { path: "*", redirectTo: "home" },
   {
     path: "home",
+    component: HomeComponent,
+    // canActivate: [AppAuthGuard]
+  },
+  {
+    path: "create",
     component: CreateComponent,
     // canActivate: [AppAuthGuard]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CreateComponent } from "./create/create.component";
 import { ControlsComponent } from "./controls/controls.component";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, CreateComponent, ControlsComponent],
+  declarations: [AppComponent, CreateComponent, ControlsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
